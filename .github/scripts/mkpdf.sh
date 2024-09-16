d93c3591625ec9a30aa8a4ff2bb7536788bff5e7
jluufigma <134333081+jluufigma@users.noreply.github.com>
2024-09-15 20:53:56 -0600

    Update mkpdf.sh

--- .github/scripts/mkpdf.sh
+++ .github/scripts/mkpdf.sh
@@ -4,11 +4,11 @@ set -e
 printf '\\newpage\n\n' > combined.md
 cat _index.md >> combined.md
 printf '\n\n' >> combined.md
-for md in gov/*.md; do
+for md in policies/*.md; do
 	printf '\n\n\\newpage\n\n' >> combined.md
 	pandoc $md \
 		--lua-filter=.github/scripts/filter.lua \
 		-f markdown-markdown_in_html_blocks \
 		-t markdown >> combined.md
 done
-pandoc combined.md --toc -V toc-title:"Figma for Government" --pdf-engine=xelatex -fmarkdown-implicit_figures -o figgov-policies.pdf
+pandoc combined.md --toc -V toc-title:"Figma Security Policies" --pdf-engine=xelatex -fmarkdown-implicit_figures -o Figma-Policies-Combined.pdf
