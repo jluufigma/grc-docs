289ccc06d5dea671b603f565a587cd96592e1a8b
jluufigma <134333081+jluufigma@users.noreply.github.com>
2024-09-15 20:53:12 -0600

    Update and rename makepol.sh to procedures.sh

--- .github/scripts/procedures.sh
+++ .github/scripts/procedures.sh
@@ -4,11 +4,11 @@ set -e
 printf '\\newpage\n\n' > combined.md
 cat _index.md >> combined.md
 printf '\n\n' >> combined.md
-for md in pol/*.md; do
+for md in procedures/*.md; do
 	printf '\n\n\\newpage\n\n' >> combined.md
 	pandoc $md \
 		--lua-filter=.github/scripts/filter.lua \
 		-f markdown-markdown_in_html_blocks \
 		-t markdown >> combined.md
 done
-pandoc combined.md --toc -V toc-title:"Figma for drthdrthdrthdtrh" --pdf-engine=xelatex -fmarkdown-implicit_figures -o figgov-a.pdf
+pandoc combined.md --toc -V toc-title:"Figma" --pdf-engine=xelatex -fmarkdown-implicit_figures -o Figma-Procedures-Combined.pdf
